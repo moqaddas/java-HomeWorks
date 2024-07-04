c9892ea57295ce4f3c5084908c7d55b848c89378
moqaddas <moqaddasr@gmail.com>
2024-07-04 07:52:54 -0700

    First Homework done.

--- src/main/java/org/example/e1/E1BooleanPrinter.java
+++ src/main/java/org/example/e1/E1BooleanPrinter.java
@@ -2,8 +2,10 @@ package org.example.e1;
 
 public class E1BooleanPrinter {
     public static void main(String[] args) {
-
-
+        boolean isTrue=true;
+        boolean isFalse=false;
+        System.out.println(isTrue);
+        System.out.println(isFalse);
 
     }
 }
--- src/main/java/org/example/e4/E4CreatingVariablesAndPrinting.java
+++ src/main/java/org/example/e4/E4CreatingVariablesAndPrinting.java
@@ -2,6 +2,11 @@ package org.example.e4;
 
 public class E4CreatingVariablesAndPrinting {
     public static void main(String[] args) {
-
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("##");
+        System.out.println("#");
+         }
     }
-}
+
