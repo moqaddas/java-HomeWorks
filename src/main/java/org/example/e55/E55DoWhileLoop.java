515c8c786a624cf3d4e7c12e0fac69ab63cf77d7
moqaddas <moqaddasr@gmail.com>
2024-07-20 12:41:25 -0700

    First Homework done.

--- src/main/java/org/example/e55/E55DoWhileLoop.java
+++ src/main/java/org/example/e55/E55DoWhileLoop.java
@@ -1,17 +1,14 @@
 package org.example.e55;
 
+import java.util.Scanner;
+
 public class E55DoWhileLoop {
     public static void main(String[] args) {
-        // Create a Scanner object to read input
-
-        // Declare a variable for user input
-
-        // Use a do-while loop to collect user input until they enter the number 7
-        // Inside the loop:
-        // Print "Enter your favorite number:"
-        // Capture the user's input and store it in the variable
-        // Print "You entered: " followed by the value of the variable
-
-        // Print "You've entered 7! Loop ends."
+        Scanner scan=new Scanner(System.in);
+        int favoriteNumber;
+        do {
+            System.out.println("Enter your favorite number");
+            favoriteNumber = scan.nextInt();
+        }while (favoriteNumber!=7);
     }
 }
