8a2b3ad6fd0795a9758942ba805e5fd9286fbff8
moqaddas <moqaddasr@gmail.com>
2024-07-25 22:22:19 -0700

    First Homework done.

--- src/main/java/org/example/e55/E55DoWhileLoop.java
+++ src/main/java/org/example/e55/E55DoWhileLoop.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class E55DoWhileLoop {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int favoriteNumber;
         do {
             System.out.println("Enter your favorite number:");
-            favoriteNumber = scan.nextInt();
+            favoriteNumber = input.nextInt();
             System.out.println("You entered: " + favoriteNumber);
         } while (favoriteNumber != 7);
         System.out.println("You've entered 7! Loop ends.");
--- src/main/java/org/example/e56/E56ForLoop.java
+++ src/main/java/org/example/e56/E56ForLoop.java
@@ -2,6 +2,8 @@ package org.example.e56;
 
 public class E56ForLoop {
     public static void main(String[] args) {
-
+        for (int count = 2; count <= 14; count += 2) {
+            System.out.println(count);
+        }
     }
 }
