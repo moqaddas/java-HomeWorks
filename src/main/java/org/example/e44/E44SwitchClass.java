b789eff3a62ee80ea291cc7da3d8e60fb17619f9
moqaddas <moqaddasr@gmail.com>
2024-07-25 22:24:40 -0700

    First Homework done.

--- src/main/java/org/example/e44/E44SwitchClass.java
+++ src/main/java/org/example/e44/E44SwitchClass.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class E44SwitchClass {
     public static void main(String[] args) {
 
-Scanner scanner=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter the type of meal (1 for breakfast, 2 for lunch, 3 for dinner).");
-        int mealType=scanner.nextInt();
+        int mealType = input.nextInt();
         double price;
-        switch (mealType){
+        switch (mealType) {
             case 1:
-                price=5.00;
+                price = 5.00;
                 break;
             case 2:
-                price=10.00;
+                price = 10.00;
                 break;
             case 3:
-                price=15.00;
+                price = 15.00;
                 break;
             default:
                 System.out.println("Invalid meal type entered");
                 return;
         }
-        System.out.println("The price of your meal is $ "+price);
+        System.out.println("The price of your meal is $ " + price);
 
     }
 }
