68612ee89a5d81912fbf33acff52b62ded1c46aa
SyntaxTechnologies <Moqaddasr@gmail.com>
2024-08-09 12:09:22 -0700

    First Homework done.

--- src/main/java/org/example/e91/E91StringManipulations.java
+++ src/main/java/org/example/e91/E91StringManipulations.java
@@ -2,12 +2,12 @@ package org.example.e91;
 
 public class E91StringManipulations {
     public static void main(String[] args) {
-        // Create a String variable named 'str1' and assign the value "syntax technologies" to it
 
-        // Create a String variable named 'str2' and assign the value "SYNTAX TECHNOLOGIES" to it
+        String str1 = "syntax technologies";
+        String str2 = "SYNTAX TECHNOLOGIES";
 
-        // Use the toUpperCase() method to convert 'str1' to uppercase and print the result
+        System.out.println(str1.toUpperCase());
+        System.out.println(str2.toLowerCase());
 
-        // Use the toLowerCase() method to convert 'str2' to lowercase and print the result
     }
 }
