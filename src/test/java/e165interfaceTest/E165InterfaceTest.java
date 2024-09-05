ef03b265138f20f43089a8be7d35cb941813c2e9
Asghar Nazir <asghar@syntaxtechs.com>
2024-09-04 16:07:49 -0800

    until E170

--- src/test/java/e165interfaceTest/E165InterfaceTest.java
+++ src/test/java/e165interfaceTest/E165InterfaceTest.java
@@ -11,7 +11,7 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
-public class E165SuperKeywordTest {
+public class E165InterfaceTest {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
